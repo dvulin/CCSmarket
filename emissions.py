c3b0ca2fdc1f33a559f6c8b52a956c52fe530394
Domagoj Vulin <domagoj.vulin@gmail.hr>
2022-09-27 19:37:20 +0200

    class skeleton

--- emissions.py
+++ emissions.py
@@ -5,3 +5,43 @@ Created on Tue Sep 27 11:58:12 2022
 @author: dvulin
 """
 
+"""
+klase:
+    emisije
+        #beccs, carbon farming
+        #industry (cement, fertilizer)
+        #storageOperator
+        #service
+        
+"""
+
+class Emissions:
+    import numpy as np
+    def __init__(self):
+        self.test = 'pass'
+          
+    def addTimeSeries(self, time_steps):
+        """
+        Parameters
+        ----------
+        time_steps : dodaje listu dana, datuma i sl., ovisno o tipu varijable
+
+        Returns
+        -------
+        None.
+
+        """
+        if type(time_steps) == 'int':
+            time_steps = np.arange(time_steps)
+            
+class Industry(Emissions):
+    def __init__(self):
+        pass
+    
+         
+        
+
+from emissions import Emissions
+e = Emissions()
+
+
